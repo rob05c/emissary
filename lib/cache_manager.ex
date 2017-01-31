e38b3fe28a776286df29eef2af2053b5f3f6c2a2
Robert Butts <robert.o.butts@gmail.com>
2017-01-30 20:28:39 -0700

    Add cache LRU logic, using ETS
    Adds Least Recently Used logic to cache, using Erlang Term Storage.
    So, when the cache reaches max_bytes, the last-requested URLs are
    purged, until bytes < max_bytes.

--- lib/cache_manager.ex
+++ lib/cache_manager.ex
@@ -3,12 +3,12 @@ defmodule Emissary.CacheManager do
 
   # \todo change to use Erlang Term Storage
   defmodule CacheData do
-    @enforce_keys [:max_bytes]
-    defstruct cache: %{}, bytes: 0, max_bytes: 0
+    @enforce_keys [:table, :lru_table, :max_bytes]
+    defstruct table: "", lru_table: "", bytes: 0, max_bytes: 0
   end
 
   def start_link(name, max_bytes) do
-    GenServer.start_link __MODULE__, max_bytes, name: name
+    GenServer.start_link __MODULE__, {name, max_bytes}, name: name
   end
 
   def get(server, url) do
@@ -20,23 +20,65 @@ defmodule Emissary.CacheManager do
     GenServer.cast server, {:set, url, val}
   end
 
-  def init(max_bytes) do
-    data = %CacheData{max_bytes: max_bytes}
+  def init({name, max_bytes}) do
+    # \todo determine if read_concurrency should be true. GenServer means only one process ever reads at a time, right?
+    :ets.new(name, [:named_table, :public, {:read_concurrency, true}])
+    lru_table = :"#{name}_lru"
+    :ets.new(lru_table, [:named_table, :ordered_set])
+
+    data = %CacheData{max_bytes: max_bytes, table: name, lru_table: lru_table}
     {:ok, data}
   end
 
+  # freshen bumps url to the top of the LRU. Should be called after someone `get`s the url.
+  def freshen(data, url) do
+    [{_, lru_index, _}] = :ets.lookup(data.table, url)
+    :ets.delete(data.lru_table, lru_index)
+
+      # \todo abstract index-and-insert duplicated in set()
+    new_lru_index = :erlang.unique_integer([:monotonic])
+    :ets.insert(data.lru_table, {new_lru_index, url})
+    :ets.update_element(data.table, url, [{2, new_lru_index}])
+    :ok
+  end
+
+  # prune deletes entries in the cache, starting with the least-recently-used, until the cache bytes is within max_bytes.
+  def prune(data) do
+    if data.bytes <= data.max_bytes do
+      data
+    else
+      eldest_lru_index = :ets.first(data.lru_table)
+      [{_, url}] = :ets.lookup(data.lru_table, eldest_lru_index)
+      [{_, _, val}] = :ets.lookup(data.table, url)
+      val_bytes = byte_size val
+      data = Map.put data, :bytes, data.bytes - val_bytes
+      :ets.delete(data.lru_table, eldest_lru_index)
+      :ets.delete(data.table, url)
+      IO.puts "cache pruned " <> url <> " size " <> Integer.to_string val_bytes
+      prune data # recursively prune until the cache is within max_bytes
+    end
+  end
+
   def handle_call({:get, url}, _from, data) do
-    cache = data.cache
-    val = Map.fetch cache, url
-    {:reply, val, data}
+    reply = case :ets.lookup(data.table, url) do
+              [{_, _, val}] ->
+                freshen(data, url)
+                {:ok, val}
+              [] ->
+                :error
+            end
+    {:reply, reply, data}
   end
 
   def handle_cast({:set, url, val}, data) do
-    cache = Map.put data.cache, url, val
-    data = Map.put data, :cache, cache
+    lru_index = :erlang.unique_integer([:monotonic])
+    :ets.insert(data.lru_table, {lru_index, url})
+    :ets.insert(data.table, {url, lru_index, val})
     val_bytes = byte_size val
-    data = Map.put data, :bytes, val_bytes
-    # \todo delete entries that exceed the cache limit
+    new_bytes = data.bytes + val_bytes
+    data = Map.put data, :bytes, new_bytes
+    IO.puts "cache inserted " <> url <> " size " <> Integer.to_string(val_bytes) <> " (" <> Integer.to_string(new_bytes) <> "/" <> Integer.to_string(data.max_bytes) <> ")"
+    data = prune data
     {:noreply, data}
   end
 
